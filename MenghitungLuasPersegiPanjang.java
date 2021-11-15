13b2ffb1fb17a27efa92cf98eae5ee9f89c46e2b
rakhmaaaa <rakhmadhaninurulaini@gmail.com>
2021-11-15 15:16:18 +0700

    first commit

---
+++ MenghitungLuasPersegiPanjang.java
@@ -0,0 +1,10 @@
+public class MenghitungLuasPersegiPanjang {
+
+    public static void main(String[] args) {
+        int panjang = 20;
+        int lebar = 30;
+        int luaspersegipanjang = panjang * lebar;
+
+        System.out.println("Luas Persegi Panjang : " + luaspersegipanjang);
+    }
+}
